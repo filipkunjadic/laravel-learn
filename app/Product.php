618a138444480bb599e4caa4417739782077047d
Filip Kunjadic - Culibrk <filip.kunjadic@pixel2go.com>
2020-08-04 22:03:35 +0200

    model relation example
    Signed-off-by: Filip Kunjadic - Culibrk <filip.kunjadic@pixel2go.com>

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $guarded = [];
+
+    // Here we wil write model relationshio
+    // public function categories(){
+    //   return $this->hasMany('App\Category');
+    // }
 }
