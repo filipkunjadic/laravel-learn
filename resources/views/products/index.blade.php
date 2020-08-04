02fa57ba50cbcebf366052a80e60bd035ca5f72f
Filip Kunjadic - Culibrk <filip.kunjadic@pixel2go.com>
2020-08-04 22:02:44 +0200

    example of how categories should be loaded
    Signed-off-by: Filip Kunjadic - Culibrk <filip.kunjadic@pixel2go.com>

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,12 @@ This is products page! :)
 <h1>Product</h1>
 @foreach($products as $product)
  <h4>{{ $product->name }}</h4>
+ {{--
+  EXAMPLE CODE
+  @foreach($product->categories as $category)
+   {{ $category->name }}
+  @endforeach
+ --}}
  <a href="{{ route('product.edit',$product->id) }}"> EDIT</a>
  <hr>
 @endforeach
